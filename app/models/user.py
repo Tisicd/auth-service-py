0e7a72858a503de2882db4fa3cc3c5fda0653bbc
David Tisalema <letisidw@gmail.com>
2025-06-23 20:48:11 -0500

    username añadido
    actualizacion para username

--- app/models/user.py
+++ app/models/user.py
@@ -1,12 +1,15 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
+from sqlalchemy import Column, String, Boolean, DateTime, func
+from sqlalchemy.dialects.postgresql import UUID
+from uuid import uuid4
+from app.db.base_class import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
+    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    role = Column(String, default="buyer")
+    is_verified = Column(Boolean, default=False)
+    created_at = Column(DateTime(timezone=True), default=func.now())
+    updated_at = Column(DateTime(timezone=True), default=func.now(), onupdate=func.now())
